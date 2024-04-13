3d53cf35766bfe90b91db3ee1426a7c00ba2c8e9
Jaoandreu <110734305+Jaoandreu@users.noreply.github.com>
2024-04-12 20:45:40 -0500

    Add initial index.html file

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -3,17 +3,16 @@ import React from 'react';
 const ContactForm = () => {
   return (
     <form name="contact" netlify>
-        <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label>Name <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Email <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+      <p>
+        <label>Name <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Email <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   );
 };
 
