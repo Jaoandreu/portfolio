5ff64922a797632830b6732cc49d86193c46da51
Jaoandreu <110734305+Jaoandreu@users.noreply.github.com>
2024-04-12 19:24:39 -0500

    Adjust form handling for Netlify

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ContactForm = () => {
   return (
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="POST" netlify>
       {/* This hidden input is crucial for Netlify to identify the form correctly */}
       <input type="hidden" name="form-name" value="contact" />
 
