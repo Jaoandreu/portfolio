a10f08dcd7deb90b4d23148e8667f5f75cc9a55f
Jaoandreu <110734305+Jaoandreu@users.noreply.github.com>
2024-04-24 21:11:56 -0500

    Testing a feature

--- src/App.js
+++ src/App.js
@@ -48,9 +48,6 @@ function App() {
       <Contact />
       <form name="contact" id="contactForm" method="Post" data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
-        <div className="form-group">
-          <input type="name" name="name" required placeholder="Name" />
-        </div>
         <div className="reach-out">Got a problem to solve?</div>
         <div className="form-group">
           <input type="email" id="email" name="email" required placeholder="Email" />
