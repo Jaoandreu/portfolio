f895b05261469476b21e7fb31a37845cd15a104b
Jaoandreu <110734305+Jaoandreu@users.noreply.github.com>
2024-04-24 21:47:39 -0500

    Testing a feature

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Projects from './Projects';
 import Description from './Description';
 import Contribution from './Contribution';
 import projectData from './data/projects.json';
-import Contact from './Contact';
+import './Contact.css';
+import Footer from './Footer';
 import Clients from './Clients';
 
 
@@ -45,18 +46,32 @@ function App() {
       </div>
       <div id="client" className="welcome-message" style={{ width: "80%", marginTop: "80px", marginBottom: "20px" }}>Trusted by the kindest clients</div>
       <Clients />
-      <Contact />
-      <form name="contact" id="contactForm" method="Post" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact" />
-        <div className="reach-out">Got a problem to solve?</div>
-        <div className="form-group">
-          <input type="email" id="email" name="email" required placeholder="Email" />
+      <div className="wrapper">
+        <div className="container-footer">
+          <form name="contact" id="contactForm" method="Post" data-netlify="true">
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="reach-out">Got a problem to solve?</div>
+            <div className="form-group">
+              <input type="email" id="email" name="email" required placeholder="Email" />
+            </div>
+            <div className="form-group">
+              <textarea id="message" name="message" rows="10" required placeholder="Message"></textarea>
+            </div>
+            <button className="submit-btn" type="submit">Send</button>
+          </form>
+          <div className="right">
+            <Footer />
+          </div>
         </div>
-        <div className="form-group">
-          <textarea id="message" name="message" rows="10" required placeholder="Message"></textarea>
+        <div className="footer-container">
+          <button onClick={() => document.getElementById('home').scrollIntoView()}>Home</button>
+          <button onClick={() => document.getElementById('portfolio').scrollIntoView()}>Portfolio</button>
+          <button onClick={() => document.getElementById('contactForm').scrollIntoView()}>Contact</button>
         </div>
-        <button className="submit-btn" type="submit">Send</button>
-      </form>
+        <div className="footer-center">
+          © 2024 - All rights reserved.
+        </div>
+      </div>
     </div>
   );
 }
