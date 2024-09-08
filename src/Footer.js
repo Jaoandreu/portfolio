c4163477cf65570f182d4dc7a9285056993c2009
Jaoandreu <110734305+Jaoandreu@users.noreply.github.com>
2024-09-08 18:13:28 -0500

    Testing a feature

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className="footer">
             <div className="footer-design"></div>
             <div className="footer-text" style={{ textAlign: 'left', paddingLeft: '10px' }}>Let's Work Together!</div>
-            <div className="footer-icon-email" style={{ textAlign: 'left', fontSize: '20px', paddingLeft: '10px' }}><i className="fas fa-envelope" style={{ paddingRight: '10px' }}></i>contact@websolutions-consultant.com</div>
+            <div className="footer-icon-email" style={{ textAlign: 'left', fontSize: '20px', paddingLeft: '10px', whiteSpace: 'nowrap'}}><i className="fas fa-envelope" style={{ paddingRight: '10px' }}></i>contact@websolutions-consultant.com</div>
             <div className="footer-icon-email" style={{ textAlign: 'left', fontSize: '20px', marginTop: '15px', paddingLeft: '10px' }}><i className="fas fa-map-marker-alt" style={{ paddingRight: '10px' }}></i> Eastern Timezone</div>
         </div >
     );
